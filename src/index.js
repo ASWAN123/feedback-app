01d6fbfc0ee875fa2449efa15e98d988a749881c
Abderrahim assoaune <aaswan048@gmail.com>
2023-03-08 19:17:45 +0100

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
@@ -12,9 +12,9 @@ const GOOGLE_LOGIN_KEY = process.env.REACT_APP_GOOGLE_LOGIN;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId={GOOGLE_LOGIN_KEY}>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
     </GoogleOAuthProvider>
 );
 
